e9b34634018293340e31640bbc2408d3d5724907
developerpd233 <91786197+developerpd233@users.noreply.github.com>
2022-10-17 15:02:09 +0500

    create a page

--- src/components/CircleSize/CircleSize.js
+++ src/components/CircleSize/CircleSize.js
@@ -4,7 +4,7 @@ import React from 'react'
 function CircleSize(props) {
   const {text}=props;
     return (
-    <div className='circle-size'><p className='para'>{text}</p></div>
+    <div className='circle-size' style={{  backgroundColor: text === 'M' && "black" , color : text === 'M' && "white" ,  }} ><p className='para'>{text}</p></div>
   )
 }
 
